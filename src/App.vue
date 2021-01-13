886b229aef3342ac8aa6abb411712748943a1a6c
panda-Mr-wan <2359944502@qq.com>
2021-01-13 19:59:52 +0800

    vue版属性检测

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>App</h3>
+    <hr>
+    <Child :num-child="num" :message="message" :data="data" :prop-f="propF" />
+  </div>
+</template>
+
+<script>
+  import Child from "./components/Child";
+  export default {
+    name:"App",
+    components:{ Child },
+    data(){
+      return {
+        num:200,
+        message:"我是帅哥",
+        data:"我是美女",
+        propF:'30'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h3>Child组件</h3>
+        <p>{{200+numChild}}</p>
+        <p>{{message}}</p>
+        <p>{{data}}</p>
+        <p>{{propF}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Child",
+        // props:['numChild']
+        props:{
+            numChild:Number,
+            message:{
+                type:String,
+                required:true
+            },
+            data:[Number,String],
+            propF:{
+                validator(value){
+                    return value>20
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
